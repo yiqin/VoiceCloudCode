d45b4a62d69eb48fa5599da1295da5768a63bb8b
yiqin <yiqin.mems@gmail.com>
2015-02-12 11:12:25 -0600

    Removed the phone verification.

--- cloud/main.js
+++ cloud/main.js
@@ -4,51 +4,6 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-
-Parse.Cloud.define("verifyPhoneNumber", function(request, response) {
-    var user = Parse.User.current();
-    var verificationCode = user.get("phoneVerificationCode");
-    if (verificationCode == request.params.phoneVerificationCode) {
-
-        var user1 = Parse.User.current();
-        user1.set("phoneVerified", true);
-        user1.set("phoneNumber", request.params.phoneNumber);
-        
-        user1.save();
-
-        response.success("Success");
-    } else {
-        response.error("Invalid verification code.");
-    }
-});
-
-
-Parse.Cloud.define("sendVerificationCode", function(request, response) {
-    var verificationCode = Math.floor(Math.random()*9999);
-    var user = Parse.User.current();
-    user.set("phoneVerified", false);
-    user.set("phoneVerificationCode", verificationCode);
-    user.set("phoneNumber", request.params.phoneNumber);
-    user.save();
-    
-    // Test
-	var twilio = require('twilio')('AC2496f56a7f2cb2785dd9e736ae990e23', '5922590783110b6493f8bc9f86bf3d20');
-	// Product
-    // var twilio = require('twilio')('AC840912e124927b5100895bc5b9a67f64', 'f83f5ef557fe90f1b1b4c6cc4d569d5c');
-    twilio.sendSms({
-        From: "+13177080548",
-        To: request.params.phoneNumber,
-        Body: "From Voice: your verification code is " + verificationCode + "."
-    }, function(err, responseData) { 
-        if (err) {
-          response.error(err);
-        } else { 
-          response.success("Success");
-        }
-    });
-});
-
-
 // Process image size....
 var Image = require("parse-image");
  
